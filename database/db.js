de12bdb4fe43aabb0fd38ffdb733fc2313dff3fa
JuanR <112212338+JuanPython1@users.noreply.github.com>
2024-02-21 16:52:30 -0500

    confirmacion del puerto

--- database/db.js
+++ database/db.js
@@ -1,9 +1,10 @@
 import {Sequelize} from 'sequelize'
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } from '../config.js'
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from '../config.js'
 
 
 const db= new Sequelize(DB_NAME, DB_USER, DB_PASSWORD,{
     host: DB_HOST,
+    port: DB_PORT,
     dialect: 'mysql'
 })
 
