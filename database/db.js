fd7246d3216f05b674cf835f9fcef690e1b5e570
JuanR <112212338+JuanPython1@users.noreply.github.com>
2024-02-21 22:32:48 -0500

    cambio de la base de datoss

--- database/db.js
+++ database/db.js
@@ -2,13 +2,8 @@ import {Sequelize} from 'sequelize'
 import { DB_HOST, DB_PASSWORD, DB_PORT, DB_USERNAME, DB_DATABASE } from '../config.js'
 
 
-const db= new Sequelize({
-    username: DB_USERNAME, 
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
+const db= new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {  
     dialect: 'mysql',
-    port: DB_PORT,
-    host: DB_HOST,
 })
 
 export default db
